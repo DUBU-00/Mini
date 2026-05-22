de22afe6eb397329db19c6e1d30d88234199ab00
DUBU-00 <psc00523@gmail.com>
2026-05-22 15:41:24 +0900

    [Update] 게임시작 로딩화면 구현

---
+++ Assets/Scripts/LoadingPopupUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LodingPopupUI : MonoBehaviour
+{
+    [SerializeField] private RawImage RawImage_Loadingimage;
+    [SerializeField] private Slider Slider_Loadingbar;
+
+    private void OnEnable()
+    {
+        LoadandImage();
+        StartCoroutine(CoStartLoadingbar());
+    }
+    private void LoadandImage()
+    {
+        var texture = Resources.Load<Texture>("Texture/Game_Loading");
+        if (texture != null )
+        {
+            RawImage_Loadingimage.texture = texture;
+        }
+    }
+    IEnumerator CoStartLoadingbar()
+    {
+        Slider_Loadingbar.value = 0.2f;
+        yield return new WaitForSeconds(0.3f);
+        Slider_Loadingbar.value = 0.4f;
+        yield return new WaitForSeconds(0.5f);
+        Slider_Loadingbar.value = 0.75f;
+        yield return new WaitForSeconds(1.0f);
+        Slider_Loadingbar.value = 1.0f;
+        yield return new WaitForSeconds(1.2f);
+
+        this.gameObject.SetActive(false);
+    }
+}
