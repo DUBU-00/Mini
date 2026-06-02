fcbb7f77475fee5cc795dce190d973df4b0a70a6
DUBU-00 <psc00523@gmail.com>
2026-06-02 20:08:31 +0900

    [Update] 보스 몬스터 무기 오브젝트 생성
    - 기존 본체일시 무기가 아닌 몸에 피격되는 현상 발생
    - bone follow로 보스무기 개별 콜라이더 생성
    - 무기에 콜라이더가 있는 상태로 무기에 붙어있어 무기 움직임 따라 공격범위가 동일

---
+++ Assets/Scripts/Boss Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BossWeapon : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+
+            BossAI boss = GetComponentInParent<BossAI>();
+
+            if (playerHealth != null && boss != null)
+            {
+                int damage = boss.GetCurrentDamage();
+
+                Vector2 hitDirection = (collision.transform.position - transform.position).normalized;
+                hitDirection.y = 0.5f;
+
+                playerHealth.TakeDamage(damage, hitDirection);
+            }
+        }
+    }
+}
