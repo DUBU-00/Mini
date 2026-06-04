9123e895aa6de39e53d9d9441e2f74cfff332f0c
DUBU-00 <psc00523@gmail.com>
2026-06-04 10:55:40 +0900

    [Update] 화면 오른쪽 하단 경험치 로그 세팅

---
+++ Assets/Scripts/Log Manager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LogManager : MonoBehaviour
+{
+    public static LogManager Instance;
+
+    [SerializeField] private Text expText;
+    [SerializeField] private float fadeDuration = 1.5f;
+    [SerializeField] private float moveSpeed = 30f;
+
+    private Coroutine fadeCoroutine;
+    private Vector3 originalPosition;
+
+    private void Awake()
+    {
+        Instance = this;
+        if (expText != null )
+        {
+            originalPosition = expText.transform.localPosition;
+            expText.gameObject.SetActive(false);
+        }
+    }
+    public void ShowExpLog(int amonut)
+    {
+
+    }
+}
